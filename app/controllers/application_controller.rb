8d8689212400fd87eff338280a03f7c117f04871
DeShawn Cousette <dcousette@gmail.com>
2016-01-20 00:00:44 -0500

    Add sign in, and basic permissions. Update nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+  
   def current_user
-    @current_user ||= User.find(session[:id]) if session[:id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
@@ -12,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_access
-    if !logged_in?
+    unless logged_in? && current_user.id == params[:id].to_i
       flash[:danger] = "Access denied!"
       redirect_to users_path
     end
