5edad6dacbd4c5232726fbce473a4c71b21530c4
DeShawn Cousette <dcousette@gmail.com>
2016-01-24 12:56:12 -0500

    update carrierwave initializer

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.production?
+  if Rails.env.production?
     config.storage    = :aws
-    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+    config.aws_bucket = ENV['S3_BUCKET_NAME']
     config.aws_acl    = 'public-read'
 
     # The maximum period for authenticated_urls is only 7 days.
@@ -14,11 +14,10 @@ CarrierWave.configure do |config|
     }
 
     config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION') # Required
+      access_key_id:     ENV['AWS_ACCESS_KEY_ID']
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      region:            ENV['AWS_REGION']
     }
-    
   else
     config.storage :file
     config.enable_processing = Rails.env.development?
