22db01bf6f514ce256c2a431a070512cd450327e
DeShawn Cousette <dcousette@gmail.com>
2016-01-24 13:31:34 -0500

    add password to signup form - require password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :songs 
+  has_many :songs
   has_secure_password
-  validates_presence_of :username, :email
+  validates_presence_of :username, :email, :password
 end
